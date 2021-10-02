46d7588816e43ac289b678197a474215ab2df9a2
luke.moon <gathering1143@gmail.com>
2021-10-02 16:24:19 +0900

    add: 도토리창고 api 연동

--- index.py
+++ index.py
@@ -1,23 +1,50 @@
 import RPi.GPIO as GPIO
 import time
 import requests
+import time
+import os
 
+print('# PLC Environment Activation')
+i = 0
+dots = 'now loading.'
+while i <= 3:
+    time.sleep(1)
+    if (i == 0):
+        os.system('clear')
+    print(dots)
+    i += 1
+    dots += '..'
+time.sleep(1)
+os.system('clear')
+
+print('-----------------------------------')
+print('0. 도메인 주소를 입력해주세요.')
+addr = input()
+
+print('1. device 토큰을 입력해주세요.')
+token = input()
+
+print('2. 생산 라인번호를 입력해주세요.')
+produceLineId = input()
+
+print('3-1. 생산 수량을 입력해주세요.')
+qty = input()
+print('3-2. 불량 수량을 입력해주세요.')
+inferiorQty = input()
 pirPin = 7
 GPIO.setmode(GPIO.BCM)
-
 GPIO.setup(pirPin, GPIO.IN)
 latest = 0
 
 # request config
-URL = 'http://192.168.0.8:8000'
-path = '/api/health'
-headers = {'Content-Type': 'application/json; charset=utf-8', 'Authorization': 'xxxxxxxxxxxxxxxxxxxxxxxx'}
-data = {'qty': 1, 'inferiorQty': 0, 'produceLineId': 1}
+path = '/device/auto'
+URL = addr + path
+headers = {'Content-Type': 'application/json; charset=utf-8', 'Authorization': 'Bearer ' + token}
+data = {'qty': qty, 'inferiorQty': inferiorQty, 'produceLineId': produceLineId}
 
 while True:
     current = GPIO.input(pirPin)
     if current == 0 & current != latest:
-        res = requests.get(URL + path, headers=headers, data=data)
-        print("data push")
+        res = requests.get(URL, headers=headers, data=data)
     latest = current
     time.sleep(0.2)
