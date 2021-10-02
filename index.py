59f4226098d4adbbcb48547cca9ba0772ffaf2ec
luke <luke@themyplanet.com>
2021-10-02 05:52:59 +0000

    add: index

--- index.py
+++ index.py
@@ -9,15 +9,15 @@ GPIO.setup(pirPin, GPIO.IN)
 latest = 0
 
 # request config
-URL = 'http://localhost:8000'
-path = '/device/auto'
+URL = 'http://192.168.0.8:8000'
+path = '/api/health'
 headers = {'Content-Type': 'application/json; charset=utf-8', 'Authorization': 'xxxxxxxxxxxxxxxxxxxxxxxx'}
 data = {'qty': 1, 'inferiorQty': 0, 'produceLineId': 1}
 
 while True:
     current = GPIO.input(pirPin)
     if current == 0 & current != latest:
-        res = requests.post(URL + path, headers=headers, data=data)
+        res = requests.get(URL + path, headers=headers, data=data)
         print("data push")
     latest = current
     time.sleep(0.2)
